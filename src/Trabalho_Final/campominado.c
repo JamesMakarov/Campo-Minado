13e77a8a3a560e2badd20c94f23eee8c4760729f
JamesMakarov <thiagonogueira@alu.ufc.br>
2025-02-13 15:51:20 -0300

    Atualizando Main

--- src/Trabalho_Final/campominado.c
+++ src/Trabalho_Final/campominado.c
@@ -4,15 +4,13 @@
 #include <conio.h>
 #include "campominado.h"
 
-// Cria uma nova célula com valores padrões
+
 Celula* CriarNova(int x, int y) {
     Celula* nova = (Celula*)malloc(sizeof(Celula));
     if (!nova) {
         printf("Erro ao alocar memória para célula.\n");
         exit(1);
     }
-
-    // Valores padrão
     nova->aberto = false;
     nova->bandeira = false;
     nova->bomba = false;
@@ -20,19 +18,19 @@ Celula* CriarNova(int x, int y) {
     nova->x = x;
     nova->y = y;
 
-    // Ponteiros nulos
+
     nova->cima = nova->baixo = nova->esq = nova->dir = NULL;
     nova->dcesq = nova->dcdir = nova->dbesq = nova->dbdir = NULL;
 
     return nova;
 }
 
-// Inicializa o tabuleiro com uma matriz de células
+
 void Inicializando_Tabuleiro(Tabuleiro *tabuleiro, int largura, int altura) {
     tabuleiro->largura = largura;
     tabuleiro->altura = altura;
 
-    // Alocando memória para o grid (matriz de células)
+
     tabuleiro->grid = (Celula**)malloc(altura * sizeof(Celula*));
     if (!tabuleiro->grid) {
         printf("Erro ao alocar memória para o tabuleiro.\n");
@@ -46,31 +44,25 @@ void Inicializando_Tabuleiro(Tabuleiro *tabuleiro, int largura, int altura) {
             exit(1);
         }
 
-        // Criando células
         for (int j = 0; j < largura; j++) {
             tabuleiro->grid[i][j] = *CriarNova(j, i);
         }
     }
-
-    // Conecta as células vizinhas
     Conectar_Vizinhos(tabuleiro);
 }
 
-// Conecta as células vizinhas usando os ponteiros
+
 void Conectar_Vizinhos(Tabuleiro *tabuleiro) {
     for (int i = 0; i < tabuleiro->altura; i++) {
         for (int j = 0; j < tabuleiro->largura; j++) {
             Celula *atual = &tabuleiro->grid[i][j];
 
-            // Cima e baixo
             if (i > 0) atual->cima = &tabuleiro->grid[i-1][j];
             if (i < tabuleiro->altura - 1) atual->baixo = &tabuleiro->grid[i+1][j];
 
-            // Esquerda e direita
             if (j > 0) atual->esq = &tabuleiro->grid[i][j-1];
             if (j < tabuleiro->largura - 1) atual->dir = &tabuleiro->grid[i][j+1];
 
-            // Diagonais
             if (i > 0 && j > 0) atual->dcesq = &tabuleiro->grid[i-1][j-1];
             if (i > 0 && j < tabuleiro->largura - 1) atual->dcdir = &tabuleiro->grid[i-1][j+1];
             if (i < tabuleiro->altura - 1 && j > 0) atual->dbesq = &tabuleiro->grid[i+1][j-1];
@@ -79,7 +71,7 @@ void Conectar_Vizinhos(Tabuleiro *tabuleiro) {
     }
 }
 
-// Libera memória do tabuleiro
+
 void Liberar_Tabuleiro(Tabuleiro *tabuleiro) {
     for (int i = 0; i < tabuleiro->altura; i++) {
         free(tabuleiro->grid[i]);
@@ -124,7 +116,7 @@ void Revelar_celulas(Tabuleiro *tabuleiro, int x, int y) {
         return; 
     }
     
-    Celula *celula = &tabuleiro->grid[y][x];
+    Celula *celula = &tabuleiro->grid[x][y];
 
     if (celula->aberto) {
         return;
@@ -147,4 +139,6 @@ void Revelar_celulas(Tabuleiro *tabuleiro, int x, int y) {
         Revelar_celulas(tabuleiro, x + 1, y + 1);  
     }
 }
+
+
 
--- src/Trabalho_Final/main.c
+++ src/Trabalho_Final/main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "campominado.h"
 
 
 int main() {
     Tabuleiro tabuleiro;
-    
-    // Cria um tabuleiro de 10x10
-    Inicializando_Tabuleiro(&tabuleiro, 20, 20);
-    printf("Tabuleiro criado com sucesso!\n");
 
-    // Exemplo: Mostrando coordenadas de algumas células
+    int largura, altura;
+    int num_bombas = 0;
+    int num_min_bombas = 7;
+    int num_max_bombas = 0;
+    
+    
+    printf("Por favor, insira a largura desejada: (Min = 5, Max = 40)\n");
+    scanf("%d", &largura);
+
+    while (largura > 40) {
+        printf("Por favor, insira a largura desejada, agora respeitando os limites (Min = 5, Max = 40)\n");
+    scanf("%d", &largura);
+    }
+
+    printf("Por favor, insira a altura desejada: (Min = 5, Max = 26)\n");
+    scanf("%d", &altura);
+
+    while (altura > 26) {
+        printf("Por favor, insira a altura desejada, agora respeitando os limites (Min = 5, Max = 26)\n");
+    scanf("%d", &altura);
+    }
+
+    num_max_bombas = ((largura+1)*(altura+1))/5;
+
+    printf("Digite a quantidade desejada de bombas (Min = %d, Max = %d)\n", num_min_bombas, num_max_bombas);
+    scanf("%d", &num_bombas);
+
+    while (num_bombas < num_min_bombas || num_bombas > num_max_bombas) {
+        printf("Digite a quantidade desejada de bombas, respeitando os limites (Min = %d, Max = %d)\n", num_min_bombas, num_max_bombas);
+        scanf("%d", &num_bombas);
+    }
+
+    Inicializando_Tabuleiro(&tabuleiro, largura, altura);
+    printf("Tabuleiro criado com sucesso!\n");
+    Distribuir_Bombas(&tabuleiro, num_bombas);
+
+    
     printf("Celula (0,0): x=%d, y=%d\n", tabuleiro.grid[0][0].x, tabuleiro.grid[0][0].y);
-    printf("Celula (19,19): x=%d, y=%d\n", tabuleiro.grid[19][19].x, tabuleiro.grid[19][19].y);
+    printf("Celula (19,19): x=%d, y=%d\n", tabuleiro.grid[largura-1][altura-1].x, tabuleiro.grid[largura-1][altura-1].y);
 
     // Libera memória
     Liberar_Tabuleiro(&tabuleiro);
-    return 0;
+    return EXIT_SUCCESS;
 }
